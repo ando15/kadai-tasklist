5330a8492a5e73c028081855f3cbfb916bcfccf3
tomoya.ando <tomoya.ando@techacademy.jp>
2022-09-15 14:45:54 +0000

    tasklist-syusei8

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @pagy,@tasks = pagy(Task.all,items: 3)
+    @pagy,@tasks = pagy(current_user.tasks,items: 3)
     
   end
 
@@ -10,11 +11,11 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
