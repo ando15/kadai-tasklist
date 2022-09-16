71aec241afef602c90d2ee8e922fb7d7a6219997
tomoya.ando <tomoya.ando@techacademy.jp>
2022-09-16 03:08:00 +0000

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_15_054043) do
+ActiveRecord::Schema.define(version: 2022_09_15_050339) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "status"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
